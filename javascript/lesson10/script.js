b5c0a070f6ff1da9179ab95c21f9fb671060f765
khanh <baokhanh.gbli@gmail.com>
2022-03-08 10:45:17 +0700

    add search image lesson

--- javascript/lesson10/script.js
+++ javascript/lesson10/script.js
@@ -1,26 +1,57 @@
 $(function () {
-  $(window).on("scroll", function () {
-    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-      loadImages();
+  loadImages(getParam(search, page));
+
+  $(window).scroll(function () {
+    if (
+      Math.round($(window).scrollTop()) + $(window).height() ==
+      $(document).height()
+    ) {
+      page++;
+      loadImages(getParam(search, page));
     }
+  });
+
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+    $template = $("template").clone()
+    $(".item-list .row").html("")
+    $template.appendTo($(".item-list .row"));
+    search = $("input").val();
+    page = 1;
+    let param = getParam(search, page);
+    console.log(param);
+    loadImages(param);
   });
 });
 
-const $item_list = $(".item-list .row");
-const itemTemplateEl = document.querySelector("template");
+let search = "cat";
 
-function loadImages(success) {
+function getParam(search, page) {
+  let param = {
+    q: search,
+    page: 1,
+  };
+
+  return $.param(param);
+}
+
+const $item_list = $(".item-list .row");
+let page = 1;
+
+function loadImages(param) {
+  console.log("load image page", page);
   fetch(
-    "https://api.unsplash.com/photos/?client_id=f7N-c7ynV9x6FAE3c1mP35-_1uRQeFNKMYlRro55XGA&page="
+    "https://pixabay.com/api/?key=26037716-84d57e3909d65193e7cd3af9e&" + param
   )
     .then((res) => res.json())
     .then((json) => {
-      $.each(json, function (index, item) {
-        const $item = $(itemTemplateEl.content.cloneNode(true));
-        $item.find("img").attr("src", item.urls.thumb);
-        $item.find(".sponsor").text(item.user.name);
+      $.each(json["hits"], function (index, item) {
+        const $item = $($("template").html());
+        $item.find("img").attr("src", item.webformatURL);
+        $item.find(".sponsor").text(item.user);
         $item.find(".like").text(item.likes);
-        $item.appendTo($item_list);
+
+        $item.appendTo($item_list).hide().fadeIn('slow');
       });
     });
 }
